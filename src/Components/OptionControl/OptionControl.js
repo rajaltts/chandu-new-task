47d81e827d0c8be7fd49f39cc4043469cd494aa4
maberte <mathieu.berte@carrier.utc.com>
2020-03-25 21:25:27 +0100

    update option control for smart discount

--- src/Components/OptionControl/OptionControl.js
+++ src/Components/OptionControl/OptionControl.js
@@ -44,7 +44,13 @@ function OptionControl(props) {
         SetDisplayDetails(true)
       else
         SetDisplayDetails(false)
-      props.onValueChanged([{Name:props.PropName, Value:UpdatedValue}])
+      let NewAssignments = [{Name:props.PropName, Value:UpdatedValue}]
+      if(GetPrice()){
+        let UnitDiscountProp = GetProperty("Sel_fUnitDiscount")
+        if(UnitDiscountProp && UpdatedValue === "TRUE")
+          NewAssignments.push({Name:props.PropName+".DISCOUNT", Value:UnitDiscountProp.Value})
+      }
+      props.onValueChanged(NewAssignments)
     }
 }
 
