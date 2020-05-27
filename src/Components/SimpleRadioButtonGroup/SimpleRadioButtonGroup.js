0967bf041780d235e1e880f8a7859b32d6acf4b1
Ashwini Bedre <ashwini.bedre@carrier.com>
2020-05-28 00:17:20 +0530

    Update the default language as English for Na Workflow.

--- src/Components/SimpleRadioButtonGroup/SimpleRadioButtonGroup.js
+++ src/Components/SimpleRadioButtonGroup/SimpleRadioButtonGroup.js
@@ -45,6 +45,15 @@ function SimpleRadioButtonGroup(props) {
                 {Prop ? Prop.Values.map((value, index) => {
                     if(props.HideNotAllowedValues && value.State===2)
                     return null;
+                    else if (props.DoNotTranslate) {
+                        return <Fragment>
+                        <label key={index} className={((value.State === 2 && Prop.AssignedValue === value.Value)?"SRBG-radio-notAllowed ":"") + GetClassName()}><input id={"ctrl"+ props.PropName+ value.Value} className="SRBG-radio" type="radio"  name={props.PropName} value={value.Value} onChange={handleChange} checked={Prop.AssignedValue === value.Value ? true: false}/>
+                        <span>{value.Attributes.Description}</span></label>
+                        {(!!value.Attributes.Note && props.vertical)?<div> {value.Attributes.Note.split("||").map((value, index)=>{
+                         return <li className={props.NoteclassName}>{value}</li>
+                        })}</div>:""}
+                        </Fragment> 
+                    } 
                     else
                     return <Fragment>
                      <label key={index} className={((value.State === 2 && Prop.AssignedValue === value.Value)?"SRBG-radio-notAllowed ":"") + GetClassName()}><input id={"ctrl"+ props.PropName+ value.Value} className="SRBG-radio" type="radio"  name={props.PropName} value={value.Value} onChange={handleChange} checked={Prop.AssignedValue === value.Value ? true: false}/><Culture id={FormatTransKey((props.PropName + "|" +value.Attributes.Description))}/></label>
