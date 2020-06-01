a4ac5433b942f625bca38e1232ecfb0c78b6b4e2
Shwet Jaiswal <Shwet.Jaiswal@carrier.com>
2020-06-01 10:35:27 +0530

    Updating correct logic

--- src/Components/SimpleRadioButtonGroup/SimpleRadioButtonGroup.js
+++ src/Components/SimpleRadioButtonGroup/SimpleRadioButtonGroup.js
@@ -50,9 +50,9 @@ function SimpleRadioButtonGroup(props) {
                     <label key={index} className={((value.State === 2 && Prop.AssignedValue === value.Value)?"SRBG-radio-notAllowed ":"") + GetClassName()}>
                         <input id={"ctrl"+ props.PropName+ value.Value} className="SRBG-radio" type="radio"  name={props.PropName} value={value.Value} onChange={handleChange} checked={Prop.AssignedValue === value.Value ? true: false}/>
                         {props.DoNotTranslate ?
-                            <Culture id={FormatTransKey((props.PropName + "|" +value.Attributes.Description))}/>
-                            :
                             <span>{value.Attributes.Description}</span>
+                            :
+                            <Culture id={FormatTransKey((props.PropName + "|" +value.Attributes.Description))}/>
                         }
                     </label>
                      {(!!value.Attributes.Note && props.vertical)?<div> {value.Attributes.Note.split("||").map((value, index)=>{
