05c55afbd55d3365a2253cb1a3a481ceea757efa
NGECAT - Madhubanti Bhattacharya <Madhubanti.bhattacharya@utc.com>
2021-01-06 13:03:55 +0530

    Add project dialog changes (#93)
    * changes
    
    * review comments resolved
    
    * review comments resolved.
    
    * fomatted.

--- src/Components/ConfirmModal/ConfirmModal.js
+++ src/Components/ConfirmModal/ConfirmModal.js
@@ -5,19 +5,20 @@ import DialogContent from '@material-ui/core/DialogContent';
 import CloseIcon from '@material-ui/icons/Close';
 import Button from '../Button';
 import translation from '../Translation';
-import {IconButton} from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import './ConfirmModal.css';
 
 const ConfirmModal = (props) => {
     const { contentClassName, title, isModalOpen, onClose, children, cancelDisabled, hideCancel, hideActions, disableCloseIcon,
-        modalWidth, overrideFooterCancelButton, id="", actionButtonList=[], errorMsg="",fullWidth=false,fullScreen = false, } = props;
+        modalWidth, overrideFooterCancelButton, id = "", actionButtonList = [], errorMsg = "", fullWidth = false, fullScreen = false,
+        footerComponent = null } = props;
     const cancelText = translation("Cancel");
 
     const onCancelButtonClick = () => {
         if (overrideFooterCancelButton) {
             overrideFooterCancelButton();
-        } else if(onClose) {
+        } else if (onClose) {
             !cancelDisabled && onClose(false);
         }
     }
@@ -31,10 +32,10 @@ const ConfirmModal = (props) => {
             id={id}
             aria-label={title}
             open={isModalOpen}
-            onClose={onClose} 
+            onClose={onClose}
             maxWidth={modalWidth}
             scroll="paper"
-            classes={{ paper: {maxHeight: "90vh"}}}
+            classes={{ paper: { maxHeight: "90vh" } }}
             disableBackdropClick
             fullWidth={fullWidth}
             fullScreen={fullScreen}
@@ -51,23 +52,25 @@ const ConfirmModal = (props) => {
                 {children}
             </DialogContent>
             {!hideActions &&
-            <div className="customModalDialogFooter">
-               {errorMsg && <div className="customModalDialogError"> {errorMsg} </div> }  
-                <DialogActions className="customModalActions">
-                    {!hideCancel &&
-                        <Button
-                            id="customModalCancel"
-                            name={cancelText}
-                            icon={faTimes}
-                            styles="eButton"
-                            onClick={onCancelButtonClick}
-                            disabled={cancelDisabled}>
-                                {cancelText}
-                        </Button>
-                    }
-                    {(actionButtonList.length > 0) && 
-                        actionButtonList.map(actionButton => {
-                            return <Button
+                <div className="customModalDialogFooter">
+                    {errorMsg && <div className="customModalDialogError"> {errorMsg} </div>}
+                    <DialogActions className="customModalActions">
+                    {!footerComponent ?
+                        <>
+                            {!hideCancel &&
+                                <Button
+                                    id="customModalCancel"
+                                    name={cancelText}
+                                    icon={faTimes}
+                                    styles="eButton"
+                                    onClick={onCancelButtonClick}
+                                    disabled={cancelDisabled}>
+                                    {cancelText}
+                                </Button>
+                            }
+                            {(actionButtonList.length > 0) &&
+                                actionButtonList.map(actionButton => {
+                                    return <Button
                                         id={actionButton.id}
                                         name={actionButton.name}
                                         icon={actionButton.icon}
@@ -77,13 +80,17 @@ const ConfirmModal = (props) => {
                                     >
                                         {actionButton.name}
                                     </Button>
-                        })
+                                })
+                            }
+                        </>
+                        :
+                        footerComponent
                     }
-                </DialogActions>
-            </div>
+                    </DialogActions>
+                </div>
             }
         </Dialog>
     );
-} 
+}
 
 export default ConfirmModal;
