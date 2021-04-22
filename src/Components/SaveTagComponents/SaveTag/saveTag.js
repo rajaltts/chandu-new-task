7e2f3b8f063e526ddd122e8388fe9a3b7aeab4bb
NGECAT - Nie FuKun <Fukun.Nie@Carrier.com>
2021-04-22 15:28:26 +0800

    when save candidate selection, use a project name which already existed, will throws error message. once switch tabs, error messge still exist (#155)
    * solve the save tag selection message issue
    
    * modified
    
    * modified

--- src/Components/SaveTagComponents/SaveTag/saveTag.js
+++ src/Components/SaveTagComponents/SaveTag/saveTag.js
@@ -9,7 +9,7 @@ import AddProject from "../AddProject/addProject";
 const SaveTag = (props) => {
     const { isModalOpen = false, hideComponent = () => { }, customerNameList = [], projectDataList = [],
         projectName = {}, customerName = {}, tagName = {}, contactName = {}, contactEmail = {},
-        contactNumber = {}, saveTagData, existingProjectValidation, errorMsg } = props;
+        contactNumber = {}, saveTagData, existingProjectValidation, errorMsg = '' } = props;
     const [selectedTab, setSelectedTab] = useState(0);
     const [menuList, setMenuList] = useState(customerNameList);
     const [exisitingProjectData, setExisitingProjectData] = useState({});
@@ -19,10 +19,13 @@ const SaveTag = (props) => {
         { tabName: translation("ExistingProject", "Existing Project") },
         { tabName: translation("NewProject", "New Project") }
     ];
-
+    const [errorMessage, setErrorMessage] = useState(errorMsg);
     useEffect(() => {
         setMenuList(customerNameList);
     }, [customerNameList]);
+    useEffect(() => {
+        setErrorMessage(errorMsg);
+    }, [errorMsg]);
 
     const createActionsButton = (disableSave) => {
         return [
@@ -73,6 +76,10 @@ const SaveTag = (props) => {
     }
 
     const handleTabChange = (event, value) => {
+        if(value == 0)
+        {
+            setErrorMessage('')
+        }
         setSelectedTab(value);
         reseTabContent();
     }
@@ -113,7 +120,7 @@ const SaveTag = (props) => {
             hideCancel={false}
             actionButtonList={createActionsButton(disableSave)}
             fullWidth
-            errorMsg={errorMsg}
+            errorMsg={errorMessage}
         >
             <TabsBuilder
                 tabs={tabs}
