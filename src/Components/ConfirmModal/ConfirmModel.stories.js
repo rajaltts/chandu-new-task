69c0a5699ec78bc16c0e189c3a7b8539ea9f04b5
NGECAT- SyedShadab.Raheman <SyedShadab.Raheman@carrier.com>
2022-05-26 22:43:10 +0530

    F7691 component story book (#390)
    * Confirm Modal Component
    
    Confirm Modal Component
    
    * code clean up
    
    code clean up
    
    * Renaming of variable
    
    Renaming of variable
    
    * clean up
    
    clean up
    
    * Removing overrideFooterCancelButton code to stories.js
    
    Removing overrideFooterCancelButton code to stories.js
    
    * Update ConfirmModal.js
    
    * Update ConfirmModal.js
    
    Co-authored-by: NGECAT-Jaiswal,Shwet <Shwet.Jaiswal@carrier.com>

---
+++ src/Components/ConfirmModal/ConfirmModel.stories.js
@@ -0,0 +1,104 @@
+import React, { useState }  from 'react'
+import ConfirmModal from './ConfirmModal'
+import './ConfirmModal.css'
+import CheckCircleRoundedIcon from '@material-ui/icons/CheckCircleOutlineRounded';
+
+export default {
+    title: 'Confirm Modal',
+    component: ConfirmModal,
+    modalWidth: {
+        control: {
+            type: 'select',
+        },
+    }
+}
+
+const ConfirmModalTemplate = ({ ...args }) => {
+    const [isClose, setIsclose] = useState(true)
+    const closeConfrimModel = () => {
+        setIsclose(!isClose)
+    }
+
+    const createActionsButton = (onButtonClick) => {
+        return [
+          {
+            id: "Yes_Confirmtag",
+            name: "Confirm",
+            onClick: onConfirm,
+          }
+        ];
+      };
+
+      const onConfirm = () => {
+          alert("Message confirmed.")
+      }
+    
+    return <ConfirmModal  {...args} isModalOpen={isClose} onClose={closeConfrimModel}  actionButtonList={createActionsButton()} headerIcon={CheckCircleRoundedIcon} />
+}
+
+const ConfirmModalTemplateForOverrideCancelButton = ({ ...args }) => {
+    const [isClose, setIsclose] = useState(true)
+    const closeConfrimModel = () => {
+        setIsclose(!isClose)
+    }
+    const overrideFooterCancelButton = () => {
+        alert("Cancel Overridden.")
+    }
+
+    const createActionsButton = (onButtonClick) => {
+        return [
+          {
+            id: "Yes_Confirmtag",
+            name: "Confirm",
+            onClick: onConfirm,
+          }
+        ];
+      };
+
+      const onConfirm = () => {
+        alert("Message confirmed.")
+    }
+
+    return <ConfirmModal  {...args}  isModalOpen={isClose} onClose={closeConfrimModel}  overrideFooterCancelButton={overrideFooterCancelButton} actionButtonList={createActionsButton()} headerIcon={CheckCircleRoundedIcon} />
+
+}
+
+export const ModalTemplate = ConfirmModalTemplate.bind({})
+ModalTemplate.args = {
+    dialogClassName:"",
+    contentClassName:"",
+    footerClassName:"",
+    title :"Confirm Modal",
+    children:'Please confirm your selection.',
+    cancelDisabled:false,
+    hideCancel:false,
+    hideActions:false,
+    disableCloseIcon:false,
+    id:'',
+    fullWidth:false,
+    fullScreen:false,
+    errorMsg:"",
+    hideHeader:false,
+    footerComponent:null
+
+}
+
+export const ModalForOverrideCancelButton = ConfirmModalTemplateForOverrideCancelButton.bind({})
+ModalForOverrideCancelButton.args = {
+    dialogClassName:"",
+    contentClassName:"",
+    footerClassName:"",
+    title :"Confirm Modal",
+    children:'Please confirm your selection.',
+    cancelDisabled:false,
+    hideCancel:false,
+    hideActions:false,
+    disableCloseIcon:false,
+    id:'',
+    fullWidth:false,
+    fullScreen:false,
+    errorMsg:"",
+    hideHeader:false,
+    footerComponent:null
+
+}
