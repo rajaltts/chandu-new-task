70fe38f2ee253a553f7882dc4251b1d643552f4e
NGECAT - Farid Boukerche <farid.boukerche@carrier.com>
2022-06-09 18:42:27 +0200

    fix: filter props against valid propTypes in dev only
     * When building for production, propTypes are not avaiable in reactComponent

--- src/v2/Components/utils/createAuthorizedProps.js
+++ src/v2/Components/utils/createAuthorizedProps.js
@@ -1,7 +1,16 @@
+/**
+ * Only used in development environment to remove warnings in console
+ * @param {reactComponent} reactComponent that have propTypes defined
+ * @param {object} ref containing props that we want to filter 
+ * @returns only valid props againt reactComponent propTypes
+ */
 export const createAuthorizedProps = (reactComponent, ref) => {
-    const authorizedProps = {}
-    for (const propName in reactComponent?.Naked?.propTypes) {
-        if(ref && ref[propName] !== undefined) authorizedProps[propName] = ref[propName]
+    if (process.env.NODE_ENV === 'development') {
+        const authorizedProps = {}
+        for (const propName in reactComponent?.Naked?.propTypes) {
+            if(ref && ref[propName] !== undefined) authorizedProps[propName] = ref[propName]
+        }
+        return authorizedProps
     }
-    return authorizedProps
+    return ref
 }
