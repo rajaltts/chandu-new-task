21a0032ee1a9366dde717f2be0e19656535eea38
NGECAT - Farid Boukerche <farid.boukerche@carrier.com>
2022-06-10 15:58:49 +0200

    fix: add name (implied ruleName) to the id

--- src/v2/Components/ButtonGroup/ButtonGroup.js
+++ src/v2/Components/ButtonGroup/ButtonGroup.js
@@ -3,14 +3,14 @@ import { ButtonGroup as MaterialButtonGroup } from '@material-ui/core'
 import { Button } from '../Button/Button'
 import { createAuthorizedProps } from '../utils/createAuthorizedProps'
 
-const ButtonGroup = ({ value, values = [], onChange, buttonProps, ...rest }) => {
+const ButtonGroup = ({ name, value, values = [], onChange, buttonProps, ...rest }) => {
     const authorizedProps = createAuthorizedProps(MaterialButtonGroup, rest)
     return (
         <MaterialButtonGroup {...authorizedProps}>
             {values.map((item, index) => (
                 <Button
-                    id={`ButtonGroup_${item.value}`}
-                    key={`ButtonGroup_${item.value}`}
+                    id={`ButtonGroup_${name}_${item.value}`}
+                    key={`ButtonGroup_${name}_${item.value}`}
                     selected={item.value === value}
                     disabled={!item.feasible || authorizedProps?.disabled}
                     onClick={() => onChange(item.value)}
