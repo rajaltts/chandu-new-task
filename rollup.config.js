57be877bf4e2cce6f2bfc70239f0b296acedd4af
NGECAT - Raphael Gomes <Raphael.Gomes@carrier.com>
2022-09-27 16:38:04 +0200

    add hot module reload

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,39 @@ import babel from 'rollup-plugin-babel'
 import image from 'rollup-plugin-image';
 import postcss from 'rollup-plugin-postcss'
 import svgr from '@svgr/rollup'
-import {terser} from 'rollup-plugin-terser'
+import { terser } from 'rollup-plugin-terser'
+
+const production = !process.env.ROLLUP_WATCH;
+
+const umd = {
+    format: 'umd',
+    name: 'ngecat-reactcomponents',
+    file: "dist/ngecat-reactcomponents.min.js",
+    globals: {
+        react: "React"
+    }
+}
+
+const esm = {
+    format: 'esm',
+    name: 'ngecat-reactcomponents',
+    file: "dist/ngecat-reactcomponents.min.js",
+    sourceMap: true,
+    globals: {
+        react: "React"
+    }
+}
 
 const config = {
     input: 'src/index.js',
     external: ['react'],
+    cache: true,
+    watch: {
+        chokidar: true,
+        clearScreen: true,
+        include: ['src/**'],
+        exclude: ['node_modules/**', 'src/__tests__', '**/*.test.js', 'dist'],
+    },
     plugins: [
         postcss(),
         babel({
@@ -16,12 +44,6 @@ const config = {
         image(),
         svgr()
     ],
-    output: {
-        format: 'umd',
-        name: 'ngecat-reactcomponents',
-        globals: {
-            react: "React"
-        }
-    }
+    output: production ? umd : esm
 }
 export default config
